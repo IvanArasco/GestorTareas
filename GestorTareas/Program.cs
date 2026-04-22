907f9252dd836bcf7cd7777443026ef89146d8a0
Ivan Arasco <arascoivan97@gmail.com>
2026-04-22 12:23:02 +0200

    Add Tests folder to GIT repo

--- GestorTareas/Program.cs
+++ GestorTareas/Program.cs
@@ -2,7 +2,9 @@
 
 List<Tarea> tareas = new List<Tarea>(capacity: 30); // 30 tareas aprox
 
+GestorTareas gestor = new GestorTareas();
 
+//gestor.ListarTareas();
 /*
  * Tarea bug = new Bug("No se ve el botón", Tarea.Prioridad.Alta, DateTime.Now.AddDays(5), "No se ve el botón", "Visualizar el botón");
 bug.Descripcion = "Ya se ve pero va regulín";
---
+++ TestGestorTareas/TareaTests.cs
@@ -0,0 +1,63 @@
+﻿using GestorDeTareas;
+
+namespace TestGestorTareas
+{
+    public class TareaTests
+    {
+        private Tarea _tareaTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _tareaTest = new Bug("Titulo Test 001", Tarea.Prioridad.Baja, DateTime.Today.AddDays(1), null);
+        }
+
+        [Test]
+        public void IniciarTarea()
+        {
+            // 2. ACT
+            _tareaTest.Iniciar();
+
+            // 3. ASSERT
+            Assert.That(_tareaTest.EstadoTarea, Is.EqualTo(Tarea.Estado.EnProgreso));
+        }
+        [Test]
+        public void CompletarTarea()
+        {
+            // 2. ACT
+            _tareaTest.Completar();
+
+            // 3. ASSERT
+            Assert.That(_tareaTest.EstadoTarea, Is.EqualTo(Tarea.Estado.Completada));
+        }
+        [Test]
+        public void CancelarTarea()
+        {
+
+            // 2. ACT
+            _tareaTest.Cancelar(_tareaTest.MotivoCancelacion);
+            _tareaTest.Cancelar(_tareaTest.Descripcion);
+
+            // 3. ASSERT
+            Assert.That(_tareaTest.EstadoTarea, Is.EqualTo(Tarea.Estado.Cancelada));
+        }
+        [Test]
+        public void EstaVencida()
+        {
+            // 2. ACT
+            _tareaTest.EstaVencida();
+
+            // 3. ASSERT
+            Assert.That(_tareaTest.EstaVencida(), Is.False);
+        }
+        [Test]
+        public void CalcularTiempoRestante()
+        {
+            // 2. ACT
+            _tareaTest.CalcularTiempoRestante();
+
+            // 3. ASSERT
+            Assert.That(_tareaTest.CalcularTiempoRestante(), Is.GreaterThan(0));
+        }
+    }
+}
