cc24d01979fb18fe5c63f8dcd49117fae6024c18
Ivan Arasco <arascoivan97@gmail.com>
2026-04-27 13:49:57 +0200

    Add email to User class and TaskManagerContext

--- GestorTareas/Domain/Entities/User.cs
+++ GestorTareas/Domain/Entities/User.cs
@@ -1,23 +1,23 @@
-﻿using static System.Net.WebRequestMethods;
-
-namespace GestorDeTareas.Domain.Entities
+﻿namespace GestorDeTareas.Domain.Entities
 {
-    internal class User
+    public class User
     {
         public string Name {  get; private set; }
+        public string Email { get; private set; }
         public DateTime Birthdate { get; private set; }
         public bool IsAdmin { get; private set; }
 
-        public User(string name, DateTime birthdate, bool isAdmin)
+        public User(string name, string email, DateTime birthdate, bool isAdmin)
         {
             Name = name;
+            Email = email;
             Birthdate = birthdate;
             IsAdmin = isAdmin;
         }
 
         public override string ToString()
         {
-            return $"[USER] Nombre : {Name} - Fecha Nacimiento: {Birthdate} - {(IsAdmin ? "Es admin" : "No es admin")}";
+            return $"[USER] Nombre : {Name} - Email: {Email} - Fecha Nacimiento: {Birthdate} - {(IsAdmin ? "Es admin" : "No es admin")}";
         }
     }
 }
---
+++ GestorTareas/Infrastructure/Data/TaskManagerContext.cs
@@ -0,0 +1,41 @@
+﻿using GestorDeTareas.Domain.Entities;
+using Task = GestorDeTareas.Domain.Entities.Task;
+using Microsoft.EntityFrameworkCore;
+
+public class TaskManagerContext : DbContext
+{
+    // Cada DbSet representa una tabla en la BD
+    public DbSet<User> Users { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // Indicar a EF Core qué proveedor usar y cómo conectarse
+        options.UseSqlServer(
+        @"Server=localhost\SQLEXPRESS;" +
+        "Database=GestorTareas;" +
+        "Trusted_Connection=True;" +
+        "TrustServerCertificate=True;"
+        );
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configurar el discriminador TPH
+        modelBuilder.Entity<Task>()
+        .HasDiscriminator<string>("TipoTarea")
+        .HasValue<RecurringTask>("Recurrente")
+        .HasValue<Bug>("Bug")
+        .HasValue<Improvement>("Mejora")
+        .HasValue<NewFeature>("Nueva funcionalidad");
+
+        // Limitar longitud del título
+        modelBuilder.Entity<Task>()
+        .Property(t => t.Title)
+        .HasMaxLength(200)
+        .IsRequired();
+        // Índice único en Email de Usuario
+        modelBuilder.Entity<User>()
+        .HasIndex(u => u.Email)
+        .IsUnique();
+    }
+}
