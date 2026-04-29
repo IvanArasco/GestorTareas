11693bb813b6c39db10d62281582c54d69e84386
Ivan Arasco <arascoivan97@gmail.com>
2026-04-29 13:37:43 +0200

    Add info to Executed SQL file to register history

--- GestorTareas/Controllers/TasksController.cs
+++ GestorTareas/Controllers/TasksController.cs
@@ -2,7 +2,6 @@
 public class TasksController : ControllerBase
 {
     private readonly TaskManagerContext _context;
-
     public TasksController(TaskManagerContext context)
     {
         _context = context;
