166ce833c50cbdce86a0549a7ee2730e81e0bd51
Ivan Arasco <arascoivan97@gmail.com>
2026-04-29 13:35:21 +0200

    Add Controllers folder, appsettings and prepare Program to build api

---
+++ GestorTareas/Controllers/TasksController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+public class TasksController : ControllerBase
+{
+    private readonly TaskManagerContext _context;
+
+    public TasksController(TaskManagerContext context)
+    {
+        _context = context;
+    }
+}
--- GestorTareas/Presentation/Program.cs
+++ GestorTareas/Presentation/Program.cs
@@ -1,80 +1,34 @@
-﻿using GestorDeTareas.Domain.Entities;
-using GestorDeTareas.Domain.Enums;
-using Task = GestorDeTareas.Domain.Entities.Task;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class Program
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// PARTE 1: registrar servicios
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TaskManagerContext>
+(options =>
+options.UseSqlServer(
+builder.Configuration
+.GetConnectionString("GestorTareas")
+)
+);
+
+var app = builder.Build();
+
+// PARTE 2: configurar el pipeline de peticiones
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        List<Task> tareas = new List<Task>(capacity: 30); // 30 tareas aprox
-
-        TaskManager gestor = new TaskManager();
-        GenerateTasks(tareas);
-        foreach (Task task in tareas)
-        {
-            Console.WriteLine(task.ToString());
-        }
-
-    }
-    private static void GenerateTasks(List<Task> tareas)
-    {
-        for (int i = 0; i < 5; i++) // Añadir Bugs
-        {
-            tareas.Add(new Bug(
-                $"Bug 00{i}",
-                Priority.Medium,
-                DateTime.Now.AddDays(5),
-                $"ComportamientoActual 00{i}",
-                $"ComportamientoEsperado 00{i}"
-               )
-            );
-        }
-
-        for (int i = 0; i < 5; i++) // Añadir Mejoras
-        {
-            tareas.Add(new Improvement(
-                $"Mejora 00{i}",
-                $"Funcionalidad afectada 00{i}",
-                $"Cambio esperado 00{i}",
-                Priority.Medium,
-                DateTime.Now.AddDays(4)
-               )
-            );
-        }
-
-        for (int i = 0; i < 5; i++) // Añadir Nuevas funcionalidades
-        {
-            tareas.Add(new NewFeature(
-                $"Nueva funcionalidad 00{i}",
-                Priority.High,
-                DateTime.Now.AddDays(3),
-                DevelopmentArea.Backend
-               )
-            );
-        }
-
-        for (int i = 0; i < 5; i++) // Añadir Tareas recurrentes
-        {
-            tareas.Add(new RecurringTask(
-                $"Tarea recurrente 00{i}",
-                Priority.Low,
-                DateTime.Now.AddDays(8),
-                Frequency.Weekly,
-                DateTime.Now,
-                DateTime.Now.AddDays(4)
-               )
-            );
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-/* gestor.ListarTareas();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
-gestor = new GestorTareas();
-gestor.Cargar("./tareas.json");
-gestor.ListarTareas();
-
-GestorTareas gestor = new GestorTareas();
-gestor.Guardar("./tareas.json");
-gestor.ListarTareas();
-*/
+app.Run(); // arranca el servidor y se queda escuchando
